dd4d90c2af1e4e8cd126863ffdc59afb949cd05c
Ibrahima Sow <isow@depaul.edu>
2025-11-11 20:16:26 -0600

    ok everything SHOULD be good, now!

--- src/test/java/playwrightTraditional/BookstoreTest.java
+++ src/test/java/playwrightTraditional/BookstoreTest.java
@@ -17,7 +17,7 @@ public class BookstoreTest {
     @BeforeAll
     public static void setUpAll() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @BeforeEach
